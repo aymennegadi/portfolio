7204100ebf8a5508097b89d4addc5135b72036b7
Aymen Negadi <aymennegadi704@gmail.com>
2025-06-20 02:43:06 +0100

    Remove calculator files and update form handling in portfolio

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-function sendMessage(){
+function sendMessage() {
   var user_name = document.getElementById("name").value;
-  var user_message = document.getElementById("email").value;
+  var user_email = document.getElementById("email").value;
   var user_tel = document.getElementById("tel").value;
 
-  var message = "Name: " + user_name + "\nEmail" + email + "\nTel: " + user_tel;
-  const bot_id  = '7890966434:AAGxUMC6QwHsuJT8a0C6N39rLIC3Pqls2G8'
+  var message =
+    "Name: " + user_name + "\nEmail: " + user_email + "\nTel: " + user_tel;
+  const bot_id = "7890966434:AAGxUMC6QwHsuJT8a0C6N39rLIC3Pqls2G8";
+  const chat_id = "5587335581"; // Replace with your actual chat_id
 
-  console.log(message);
-
-   fetch("https://api.telegram.org/bot" +bot_id + "/sendMessage"),{
+  fetch("https://api.telegram.org/bot" + bot_id + "/sendMessage", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
-   },
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       chat_id: chat_id,
-      text: message
+      text: message,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Message sent:", data);
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("tel").value = "";
     })
-  .then(response => response.json())
-  .then(data => {
-    console.log(message);
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("tel").value = "";
-
-    .catch(error => {
-       console.error("Error sending message", error);
-  });
-});
+    .catch((error) => {
+      console.error("Error sending message", error);
+    });
 
   return false;
 }
